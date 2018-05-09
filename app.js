5abb0a54dd40657c9e51c70cf417212500e8b6d9
Miguel Dias <migueld@br.ibm.com>
2018-05-09 15:44:59 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict"
+
+let nodePong = require('node-pong');
+console.log('server is running');
---
+++ gitSetupIBMUser.sh
@@ -0,0 +1,5 @@
+
+#Define the user for this project as my IBM user
+
+git config user.name "Miguel Dias"
+git config user.email migueld@br.ibm.com
