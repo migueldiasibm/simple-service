2a9e5c4b99cd7c8b53498e1d08cc6fe97de62a89
Miguel Dias <migueld@br.ibm.com>
2018-05-22 15:49:53 -0300

    adding manifest.yml

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 "use strict"
 
+var http = require('http');
+
 let nodePong = require('node-pong');
-console.log('server is running');
+let serverMessage = 'Server is running';
+let port = 80;
+
+let server = http.createServer(processServer).listen(port);
+
+function processServer(req, res) {
+    if (req.url.indexOf('/') > -1) {
+        res.setHeader('Accept-Charset', 'utf-8');
+        res.setHeader('endpoint', '/ping');
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Simple service</title></head><body><h1>The simple service is running.</h1></body></html>');
+    }
+    res.end();
+}
+
+//onsole.log('server is running');
+console.log(serverMessage + port);
